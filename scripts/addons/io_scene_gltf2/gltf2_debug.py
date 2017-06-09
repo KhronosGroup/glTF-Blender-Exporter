259a69cee3cf0bf63d7de54a380e1389d5878791
Norbert Nopper <nopper@ux3d.io>
2017-06-09 11:40:03 +0200

    Added profiling functions.

--- scripts/addons/io_scene_gltf2/gltf2_debug.py
+++ scripts/addons/io_scene_gltf2/gltf2_debug.py
@@ -16,9 +16,16 @@
 # Imports
 #
 
+import time
+
 #
 # Globals
 #
+
+g_profile_started = False
+g_profile_start = 0.0
+g_profile_end = 0.0
+g_profile_delta = 0.0
 
 #
 # Functions
@@ -31,3 +38,47 @@ def print_console(level,
 
 def print_newline():
     print()
+
+
+def print_timestamp(output = None):
+    final_output = str(time.time())
+    
+    if output is not None:
+        final_output = output + ' ' + final_output
+    
+    print_console('TIMESTAMP', final_output)
+    
+
+def profile_start():
+    global g_profile_start
+    global g_profile_started
+    
+    if g_profile_started:
+        print_console('ERROR', 'Profiling already started')
+        return
+        
+    g_profile_started = True
+    
+    g_profile_start = time.time()
+
+
+def profile_end(label = None):
+    global g_profile_end
+    global g_profile_delta
+    global g_profile_started
+    
+    if not g_profile_started:
+        print_console('ERROR', 'Profiling not started')
+        return
+    
+    g_profile_started = False
+
+    g_profile_end = time.time()
+    g_profile_delta = g_profile_end - g_profile_start
+    
+    output = str(g_profile_delta)
+    
+    if label is not None:
+        output = output + '(' + label + ')'    
+
+    print_console('PROFILE', output)
